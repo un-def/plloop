a662999da4efceea75d466008cfa64da7179a62a
un.def <un.def@ya.ru>
2016-04-18 12:34:41 +0300

    tests

--- tests.lua
+++ tests.lua
@@ -59,4 +59,94 @@ TestClassMeta = {
 }
 
 
+TestMethods = {
+
+   setUp = function(self)
+      self.class = plloop.create_class('MyClass', {
+         __init__ = function(self, initial)
+            self.value = initial
+         end
+         ,
+         get_value = function(self)
+            return self.value
+         end
+         ,
+         set_value = function(self, value)
+            self.value = value
+         end
+      })
+   end
+   ,
+   testInitMethod = function(self)
+      local obj = self.class(10)
+      luaunit.assertEquals(obj.value, 10)
+   end
+   ,
+   testRegularMethodSetter = function(self)
+      local obj = self.class()
+      obj.set_value(20)
+      luaunit.assertEquals(obj.value, 20)
+   end
+   ,
+   testRegularMethodGetter = function(self)
+      local obj = self.class(30)
+      luaunit.assertEquals(obj.get_value(), 30)
+   end
+   ,
+   testDynamicallyAddedRegularMethod = function(self)
+      local obj = self.class(40)
+      self.class.get_double_value = function(self)
+         return self.value * 2
+      end
+      luaunit.assertEquals(obj.get_double_value(), 80)
+   end
+   ,
+   testOverloadedRegularMethod = function(self)
+      local obj = self.class(50)
+      self.class.get_value = function(self)
+         return self.value + 1
+      end
+      luaunit.assertEquals(obj.get_value(), 51)
+   end
+
+}
+
+
+TestMetaMethods = {
+
+   setUp = function(self)
+      self.class_tostring_default = plloop.create_class(
+         'TostringDefaultClass', {})
+      self.class_tostring_custom = plloop.create_class(
+         'TostringCustomClass',
+         {
+            __tostring = function(self)
+               return ('[%s:%s]'):format(self.__class__.__name__, self.__id__)
+            end
+         }
+      )
+   end
+   ,
+   testDefaultTostringMethod = function(self)
+      local obj = self.class_tostring_default()
+      luaunit.assertStrMatches(
+         tostring(obj), '<TostringDefaultClass instance: 0x%w+>')
+   end
+   ,
+   testCustomTostringMethod = function(self)
+      local obj = self.class_tostring_custom()
+      luaunit.assertStrMatches(tostring(obj), '%[TostringCustomClass:0x%w+%]')
+   end
+   ,
+   testOverloadedTostringMethod = function(self)
+      self.class_tostring_custom.__tostring = function(self)
+         return ('<Overloaded:%s>'):format(self.__id__)
+      end
+      local obj = self.class_tostring_custom()
+      luaunit.assertStrMatches(tostring(obj), '<Overloaded:0x%w+%>')
+   end
+
+}
+
+
 os.exit(luaunit.LuaUnit.run())
